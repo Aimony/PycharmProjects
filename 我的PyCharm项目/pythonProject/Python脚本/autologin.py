1d2625bd6cac68dff1d207331cde6b888f225f00
Aimony <86666479+Aimony@users.noreply.github.com>
2022-03-19 00:02:37 +0800

    Update autologin.py

--- 我的PyCharm项目/pythonProject/Python脚本/autologin.py
+++ 我的PyCharm项目/pythonProject/Python脚本/autologin.py
@@ -3,10 +3,10 @@ import requests
 import requests
 url=" http://59.77.64.130/eportal/InterFace.do?method=login"
 data = {
-"userId": 20210340519,
-"password": 240536,
+"userId": 账号,
+"password": 密码,
 "service": "lydx",
-"queryString": "wlanuserip%3Dde1715e6f6af587706945ad5f90c9294%26wlanacname%3D7d649d4e68d96be1%26ssid%3D%26nasip%3D364e6faa21ed2c5ebcf404b297b78b3e%26snmpagentip%3D%26mac%3Dadffc1212f48d931a5901ee21518d9f7%26t%3Dwireless-v2%26url%3Df01d046ea18fc54eb5d7b94c8f83672c91f86b6dbbf5b202%26apmac%3D%26nasid%3D7d649d4e68d96be1%26vid%3D204195187f80517b%26port%3Da9b5f76e35a17d09%26nasportid%3D1b54fdc68c274264daf5c364443f2aedb6b6bb5eebdd906c0a65861b61339490"
+"queryString": "响应体"
 }
 r = requests.post(url,data=data).status_code
 print(r)
